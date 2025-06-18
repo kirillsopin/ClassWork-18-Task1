f45450e3662994117d3611a032f75e4c148db37c
kirillsopin <sopinkirill001@gmail.com>
2025-06-18 19:15:57 +0300

    Add some PC

--- ClassWork 18 Task1.cpp
+++ ClassWork 18 Task1.cpp
@@ -1,8 +1,15 @@
 ﻿#include <iostream>
+#include "Computer.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n = 5;
+    computer* arr = new computer[n]{
+        {"Asus ROG 5", 8, 15'999},
+        {"Lenovo Odisey", 9, 20'999},
+        {"Samsung", 3, 9'000},
+        {"Asus ROG 5", 6, 15'999}
+    };
 }
 
 
